d21f8dd641f5df04a0ffe72bdbe1c144fc72f3b1
Rishal <Rishal.mhd009@gmail.com>
2023-05-13 03:20:29 +0530

    component splitting done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExprenseItem from "./components/ExpenseItem.js";
+import ExpenseItem from "./components/ExpenseItem.js";
 
 function App() {
   const expenses = [
@@ -25,22 +25,22 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExprenseItem
+      <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
       />
-      <ExprenseItem
+      <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
       />
-      <ExprenseItem
+      <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
       />
-      <ExprenseItem
+      <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
